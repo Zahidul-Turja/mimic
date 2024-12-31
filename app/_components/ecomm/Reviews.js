40a66f4a54d22797ea937ce391ee699ed1266322
Zahidul-Turja <zahidulturja@gmail.com>
2024-12-31 18:43:33 +0600

    Enhance Reviews component layout and styling; improve rating display and review formatting

--- app/_components/ecomm/Reviews.js
+++ app/_components/ecomm/Reviews.js
@@ -2,31 +2,38 @@ import { FaStar } from "react-icons/fa";
 
 function Reviews({ reviews, rating }) {
   return (
-    <div className="my-4 w-full rounded-md bg-primary-900 px-12 py-12">
-      <h1>Reviews</h1>
+    <div className="mx-auto my-4 w-full rounded-md bg-primary-900 px-12 py-12">
+      <h1 className="my-4 text-2xl font-semibold tracking-wide">Reviews</h1>
       <div className="flex items-center gap-2">
-        <span className="flex items-center gap-1 text-base text-yellow-400">
-          <FaStar className="text-xs" />
-          <span>{rating}</span>
+        <span className="my-4 flex items-center gap-1 text-2xl">
+          {Array.from({ length: rating }, (_, i) => (
+            <FaStar key={i} className="text-yellow-400" />
+          ))}
+          {Array.from({ length: 6 - rating }, (_, i) => (
+            <FaStar key={i} className="text-yellow-100" />
+          ))}
+          <span className="mx-4 text-gray-300">
+            {rating} <span className="text-xl text-gray-500">/ 5</span>
+          </span>
         </span>
         {/* <h3 className="text-sm font-extralight text-gray-300">
           Rated by {reviews.length} users
         </h3> */}
       </div>
-      <div className="my-4">
+      <div className="mx-auto my-4">
         {reviews.map((review, index) => (
           <div
             key={index}
-            className="my-4 rounded-md bg-primary-950 px-8 py-4 text-left"
+            className="my-8 rounded-md bg-primary-950 px-8 py-4 text-left"
           >
-            <div className="flex items-center justify-between">
+            <div className="-mb-1 flex items-center justify-between">
               <p className="font-bold">{review.reviewerName}</p>
               <p className="text-sm font-light">{review.date.split("T")[0]}</p>
             </div>
             <p className="text-sm font-extralight">
               {review.reviewerEmail.split("@")[0]}
             </p>
-            <div className="flex items-center gap-2">
+            <div className="mb-2 flex items-center gap-2">
               {Array.from({ length: review.rating }, (_, i) => (
                 <FaStar key={i} className="text-yellow-400" />
               ))}
@@ -34,9 +41,11 @@ function Reviews({ reviews, rating }) {
                 <FaStar key={i} className="text-yellow-100" />
               ))}
 
-              <h2 className="text-lg font-bold">{review.rating}</h2>
+              {/* <h2 className="text-lg font-normal text-yellow-400">
+                {review.rating}
+              </h2> */}
             </div>
-            <p>{review.comment}</p>
+            <p className="my-4 text-lg">{review.comment}</p>
           </div>
         ))}
       </div>
