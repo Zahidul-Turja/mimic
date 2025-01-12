24912fc608cd0bd2c9f9f00936fc4be703c25466
Zahidul-Turja <zahidulturja@gmail.com>
2025-01-12 17:06:34 +0600

    Implement sidebar navigation with toggle functionality and category links

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,9 +1,70 @@
+"use client";
+
+import { useState, useRef, useEffect } from "react";
 import { IoMenuSharp } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
+
+import { categories } from "@/public/data";
+import Link from "next/link";
 
 function Navigation() {
+  const [open, setOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  function toggle() {
+    setOpen(!open);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setOpen(false); // Close the sidebar if clicked outside
+      }
+    };
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open]);
+
   return (
     <div>
-      <IoMenuSharp className="cursor-pointer text-3xl" />
+      <IoMenuSharp className="cursor-pointer text-3xl" onClick={toggle} />
+
+      <div
+        ref={sidebarRef}
+        className={`4 fixed top-0 z-50 h-screen bg-primary-50 px-7 py-20 text-primary-950 transition-all duration-500 ${
+          open ? "right-0 rounded-none" : "-right-full rounded-l-3xl"
+        }`}
+      >
+        <RxCross2
+          className="absolute right-10 top-10 cursor-pointer text-3xl"
+          onClick={toggle}
+        />
+        <div className="flex flex-col gap-4">
+          <Link
+            href={"/"}
+            onClick={toggle}
+            className="cursor-pointer border-b border-gray-500 px-12 py-4 text-center text-xl font-semibold transition-all duration-200 hover:rounded-lg hover:bg-slate-900/70 hover:text-primary-100"
+          >
+            Home
+          </Link>
+          {categories.map((category) => (
+            <Link
+              key={category.id}
+              href={category.href}
+              onClick={toggle}
+              className="cursor-pointer border-b border-gray-500 px-12 py-4 text-center text-lg font-bold transition-all duration-200 hover:rounded-lg hover:bg-slate-900/70 hover:text-primary-100"
+            >
+              {category.name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
