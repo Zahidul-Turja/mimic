d49a447ea627db213c3be2a5a3ae5f9102fd770a
Zahidul-Turja <zahidulturja@gmail.com>
2025-01-13 17:07:29 +0600

    Add payment modal to CheckoutCard for improved user experience

--- app/_components/ecomm/CheckoutCard.js
+++ app/_components/ecomm/CheckoutCard.js
@@ -5,9 +5,15 @@ import { useRouter } from "next/navigation";
 import { LiaShippingFastSolid } from "react-icons/lia";
 
 import { clearCartLocal } from "@/app/_lib/ecomm-services";
+import { useState } from "react";
 
 function CheckoutCard({ products, user, total, discountedTotal }) {
+  const [paymentOpen, setPaymentOpen] = useState(false);
   const router = useRouter();
+
+  function handleToggle() {
+    setPaymentOpen((cur) => !cur);
+  }
 
   return (
     <div className="w-full overflow-auto rounded-lg bg-primary-900 p-4 text-left md:p-8">
@@ -115,13 +121,63 @@ function CheckoutCard({ products, user, total, discountedTotal }) {
         </Link>
         <button
           className="relative rounded-lg bg-gradient-to-t from-green-900 to-green-700 px-6 py-2 text-lg font-semibold"
-          onClick={() => {
-            clearCartLocal();
-            router.push("/ecomm/cart/success");
-          }}
+          onClick={handleToggle}
         >
           Check out
         </button>
+      </div>
+
+      <div
+        className={`fixed left-0 top-0 z-30 h-screen w-screen ${!paymentOpen && "hidden"}`}
+        onClick={handleToggle}
+      >
+        <div className="absolute left-0 top-0 h-full w-full bg-primary-600/50"></div>
+        <div
+          className="md:[w-40%] absolute left-[50%] top-[50%] w-[90%] -translate-x-[50%] -translate-y-[50%] rounded-lg bg-primary-100 px-6 py-4 text-primary-800 lg:w-[25%]"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <h1 className="text-center text-xl font-bold">Payment</h1>
+          <input
+            type="text"
+            value={user?.bank.cardNumber}
+            disabled
+            className="my-2 w-full cursor-not-allowed rounded-lg bg-gray-600/40 px-4 py-2 text-gray-600"
+          />
+          <div className="my-2 flex w-full items-center justify-between gap-4">
+            <input
+              type="text"
+              value={user?.bank.cardExpire}
+              disabled
+              className="w-[50%] cursor-not-allowed rounded-lg bg-gray-600/40 px-4 py-2 text-gray-600"
+            />
+            <input
+              type="text"
+              value={user?.bank.cardNumber.slice(-3)}
+              disabled
+              className="w-[50%] cursor-not-allowed rounded-lg bg-gray-600/40 px-4 py-2 text-gray-600"
+            />
+          </div>
+
+          <div className="mt-4 flex items-center justify-end gap-2">
+            <button
+              className="rounded-md border-2 border-gray-600 px-4 py-1"
+              onClick={handleToggle}
+            >
+              Cancle
+            </button>
+            <button
+              className="rounded-md border-2 border-green-800 bg-green-800 px-4 py-1 text-primary-100"
+              onClick={() => {
+                clearCartLocal();
+                router.push("/ecomm/cart/success");
+              }}
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
